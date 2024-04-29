ea10df6b9b05c65dfc51e6971732c81f1b088c0d
Bruno Borchardt <bruno.borchardt@tuhh.de>
2024-04-29 12:42:16 +0200

    mit buchstaben tippen

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,73 @@
+use rand::{rngs::ThreadRng, Rng};
 use std::time::{Duration, Instant};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
+    scale: f32,
+
+    hidden_message: String,
+    #[serde(skip)]
+    new_message: String,
+
     #[serde(skip)]
     start_time: Instant,
-    hidden_message: String,
     waiting_time: f32,
     #[serde(skip)]
     done_waiting: bool,
+
+    characters_to_type: usize,
+    #[serde(skip)]
+    characters_typed: usize,
+    #[serde(skip)]
+    curr_character_to_type: Option<egui::Key>,
+    #[serde(skip)]
+    done_typing: bool,
+    #[serde(skip)]
+    char_generator: ThreadRng,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            start_time: Instant::now(),
+            scale: 3.0,
+
             hidden_message: "SupersicherPasswort123".to_owned(),
-            waiting_time: 2.7,
+            new_message: String::new(),
+
+            start_time: Instant::now(),
+            waiting_time: 1.0,
             done_waiting: false,
+
+            characters_to_type: 20,
+            characters_typed: 0,
+            curr_character_to_type: None,
+            done_typing: false,
+            char_generator: rand::thread_rng(),
         }
     }
 }
+
+fn reason_message_is_bad(msg: &str) -> Option<&str> {
+    if msg.len() < 10 {
+        return Some("Zu kurz");
+    }
+    if msg.chars().filter(|c| c.is_numeric()).count() < 2 {
+        return Some("Zu wenig Ziffern");
+    }
+    if msg.chars().filter(|c| c.is_uppercase()).count() < 2 {
+        return Some("Zu wenig Großbuchstaben");
+    }
+    if msg.chars().filter(|c| c.is_lowercase()).count() < 2 {
+        return Some("Zu wenig Kleinbuchstaben");
+    }
+    if msg.chars().filter(|c| !c.is_alphanumeric()).count() < 2 {
+        return Some("Zu wenig Sonderzeichen");
+    }
+
+    None
+} 
 
 impl TemplateApp {
     /// Called once before the first frame.
@@ -50,10 +96,16 @@ impl eframe::App for TemplateApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
+
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
-            egui::widgets::global_dark_light_mode_buttons(ui);
+            ui.horizontal(|ui| {
+                egui::widgets::global_dark_light_mode_buttons(ui);
+                ui.label("     Zoom: ");
+                ui.add(egui::DragValue::new(&mut self.scale).clamp_range(0.2..=2.0));
+            });            
+            ctx.set_pixels_per_point(self.scale * 3.0);
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -63,30 +115,85 @@ impl eframe::App for TemplateApp {
             let diff = now.duration_since(self.start_time);
             let waiting_time = Duration::from_secs((self.waiting_time * 60.0) as u64);
             self.done_waiting |= diff > waiting_time;
-
-            if self.done_waiting {
-                ui.heading("Luis muss nicht mehr warten :)");
-                ui.horizontal(|ui| {
-                    ui.label("Hier bitte: ");
-                    ui.text_edit_singleline(&mut self.hidden_message);
-                });
-
-                ui.add(
-                    egui::Slider::new(&mut self.waiting_time, 1.0..=15.0)
-                        .text("Wartezeit für Zukunft (min)"),
-                );
-            } else {
+            
+            if !self.done_waiting {
                 ui.horizontal(|ui| {
                     ui.add(egui::widgets::Spinner::new());
                     let time_left = waiting_time - diff;
-                    ui.heading(format!(
-                        "Luis muss noch warten (für {} Sekunden)",
+                    ui.label(format!(
+                        "Luis muss noch {} Sekunden warten. ☕",
                         time_left.as_secs() + 1
                     ));
                 });
+                ui.add_space(20.0);
             }
 
-            ui.separator();
+            if self.curr_character_to_type.is_none() {
+                const LEN: usize = 26;
+                use egui::Key::*;
+                const KEYS: [egui::Key; LEN] = [
+                    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+                ];
+                let index: usize = self.char_generator.gen_range(0..LEN);
+                self.curr_character_to_type = Some(KEYS[index]);
+            }
+            if let Some(key) = self.curr_character_to_type {
+                if !self.done_typing {
+                    let nr_left = self.characters_to_type - self.characters_typed;
+                    ui.label(format!(
+                        "Luis muss noch {} Buchstaben tippen. 🖮  Nächster: {}",
+                        nr_left,
+                        key.symbol_or_name()
+                    ));
+                }
+
+                ui.input(|info| {
+                    if info.key_pressed(key) {
+                        self.characters_typed += 1;
+                        self.curr_character_to_type = None;
+                        if self.characters_typed >= self.characters_to_type {
+                            self.done_typing = true;
+                        }
+                    }
+                });
+            }
+
+            if self.done_waiting && self.done_typing {
+                ui.label("Luis muss nicht mehr warten und nicht mehr tippen. 🎉  🎊  🎆  🎇");
+                
+                ui.add_space(50.0);
+
+                ui.horizontal(|ui| {
+                    ui.label("Hier bitte: ");
+                    ui.add(egui::Label::new(&self.hidden_message).selectable(false));
+                });
+
+                ui.add_space(20.0);
+                ui.horizontal(|ui| {
+                    ui.label("Passwort ändern: ");
+                    ui.text_edit_singleline(&mut self.new_message);
+                    if let Some(err) = reason_message_is_bad(&self.new_message) {
+                        if self.new_message.len() > 0 {
+                            let warning = egui::RichText::new(err).color(egui::Color32::RED).strong();
+                            ui.label(warning);
+                        }
+                    }
+                    else if ui.button("Übernehmen").clicked() {
+                        self.hidden_message.clone_from(&self.new_message);
+                    }
+                });
+
+                ui.add_space(20.0);
+                ui.add(
+                    egui::Slider::new(&mut self.waiting_time, 0.1..=15.0)
+                        .text("Wartezeit in der Zukunft (min)"),
+                );
+                ui.add_space(20.0);
+                ui.add(
+                    egui::Slider::new(&mut self.characters_to_type, 5..=250)
+                        .text("Anzahl Buchstaben in der Zukunft"),
+                );
+            }
         });
     }
 }
